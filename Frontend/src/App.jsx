df7453666c7a3899ec40fc9391c906ed75ab1f7a
Tejas Babar <tejas.b@inmedbharat.com>
2025-04-09 16:29:02 +0530

    react routing added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,28 +1,13 @@
 import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
 import './index.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     <div className=" h-screen bg-custom-gradient flex  flex-row items-center justify-center " >
-     <button className='w-[230px] h-[56px] bg-btn-gradient rounded-lg  text-neutral-100 text-2xl font-general-sans font-bold'>
-     Join us 
-     </button>
-     <button className='w-[230px] h-[56px] bg-btn-gradient rounded-lg  text-neutral-100 text-2xl font-general-sans font-medium'>
-     Join us 
-     </button>
-     <button className='w-[230px] h-[56px] bg-btn-gradient rounded-lg  text-neutral-100 text-2xl font-manrope font-regular'>
-     Join us 
-     </button>
-     
-      
-      
-      </div> 
-     
-    
+      <Outlet/>
     </>
   )
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import HomePage from './components/HomePage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
