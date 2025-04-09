60cd6a7a80809d2c2fd2d290b0d83d1ae014e5fd
Tejas Babar <tejas.b@inmedbharat.com>
2025-04-09 12:52:05 +0530

    color palete added gradient greated and fonts added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import './index.css'
 
 
 function App() {
@@ -6,8 +7,22 @@ function App() {
 
   return (
     <>
-     <div className=" bg-red-500" >Ganpati bappa morya!</div> 
+     <div className=" h-screen bg-custom-gradient flex  flex-row items-center justify-center " >
+     <button className='w-[230px] h-[56px] bg-btn-gradient rounded-lg  text-neutral-100 text-2xl font-general-sans font-bold'>
+     Join us 
+     </button>
+     <button className='w-[230px] h-[56px] bg-btn-gradient rounded-lg  text-neutral-100 text-2xl font-general-sans font-medium'>
+     Join us 
+     </button>
+     <button className='w-[230px] h-[56px] bg-btn-gradient rounded-lg  text-neutral-100 text-2xl font-manrope font-regular'>
+     Join us 
+     </button>
      
+      
+      
+      </div> 
+     
+    
     </>
   )
 }
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,8 +5,40 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Primary-Blue-50':'#EFF9FF',
+        'Primary-Blue-100':'#DEF1FF',
+        'Primary-Blue-200':'#B6E6FF',
+        'Primary-Blue-300':'#75D4FF',
+        'Primary-Blue-400':'#2CBEFF', 
+        'Primary-Blue-500':'#00A4F4',
+        'Primary-Blue-600':'#0084D4',
+        'Primary-Blue-700':'#0069AB',
+        'Primary-Blue-800':'#00598D',
+        'Primary-Blue-900':'#064A74',
+        'Primary-Blue-950':'#042F4D',
+        'Neutral-50':'#F6F6F6',
+        'Neutral-100':'#E7E7E7',
+        'Neutral-200':'#D1D1D1',
+        'Neutral-300':'#B0B0B0',
+        'Neutral-400':'#888888',
+        'Neutral-500':'#6D6D6D',
+        'Neutral-600':'#5D5D5D',
+        'Neutral-700':'#4F4F4F',
+        'Neutral-800':'#454545',
+        'Neutral-900':'#3D3D3D',
+        'Neutral-950':'#000000',
+      },
+      backgroundImage: {
+        'custom-gradient': 'var(--my-gradient)', // this makes it usable like `bg-custom-gradient`
+        'btn-gradient': 'var( --btn-gradient)',
+    },
+    fontFamily: {
+      'general-sans': ['"General Sans"', 'sans-serif'],
+      'manrope': ['"Manrope"', 'sans-serif'],
+    },
   },
   plugins: [],
 }
-
+}
