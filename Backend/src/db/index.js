7e3abf14b6b2b0afca3de775dae26fed547a0568
Swapnil Shinde <swapnil.s@inmedbhart.com>
2025-04-17 11:35:29 +0530

    setup complete

---
+++ Backend/src/db/index.js
@@ -0,0 +1,20 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+import { DB_NAME } from '../constants.js';
+
+dotenv.config();
+// Create a connection pool to the MySQL database
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: DB_NAME,
+    port: process.env.DB_PORT,
+    connectionLimit: 10,
+    waitForConnections: true,
+    queueLimit: 0,
+}).promise();
+
+
+export default db;
+
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,1 +1,17 @@
-console.log("setup done")
+import app from "./app.js";
+import db from "./db/index.js";
+import dotenv from 'dotenv';
+dotenv.config();
+
+try {
+  const connection = await db.getConnection();
+  console.log("Connected to the database!");
+  connection.release();
+
+  app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+} catch (err) {
+  console.error("Failed to connect to the database:", err);
+  process.exit(1); // Exit the process if DB connection fails
+}
