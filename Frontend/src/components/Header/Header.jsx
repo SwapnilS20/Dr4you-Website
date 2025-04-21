15ce92b2c725867cd86cefc17d05d1f5556122ac
Tejas Babar <tejas.b@inmedbharat.com>
2025-04-21 17:40:22 +0530

    responsiveness for mobile added

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <div>
             <div className=' flex justify-between items-center p-8 pl-12 pr-12 '>
                 <div className='text-xl font-general-sans font-bold'>Drs-4You</div>
-                <nav className=' space-x-16 text-Neutral-900  font-manrope font-semibold text-[16px] '>
+                <nav className=' hidden md:flex space-x-16 text-Neutral-900  font-manrope font-semibold text-[16px] '>
                     <a href="#" className='text-lg'>Home</a>
                     <a href="#" className='text-lg'>Services</a>
                     <a href="#" className='text-lg'>Find Doctors</a>
@@ -16,7 +16,16 @@ const Header = () => {
                     <a href="#" className='text-lg'>Contact Us</a>
 
                 </nav>
-                <Button styles={' h-[56px] , w-[174px] , text-[20px] '} children={" Patient Login"}/>
+                <div className=' flex  justify-center md:hidden items-center rounded-xl h-[50px] w-[48px] bg-Primary-Blue-600'>
+                    <div className=' grid grid-cols-2  gap-1  '>
+                        <span className=' h-[9px] w-[9px] rounded-sm border-2 border-white'></span>
+                        <span className=' h-[9px] w-[9px] rounded-sm border-2 border-white'></span>
+                        <span className=' h-[9px] w-[9px] rounded-sm border-2 border-white'></span>
+                        <span className=' h-[9px] w-[9px] rounded-sm border-2 border-white'></span>
+
+                    </div>
+                </div>
+                <Button styles={' h-[56px] , w-[174px] , text-[20px], hidden , md:flex , text-[20px] , justify-center , items-center'} children={" Patient Login"}/>
             </div>
         </div>
         
--- Frontend/src/components/HeroSection/Hero.jsx
+++ Frontend/src/components/HeroSection/Hero.jsx
@@ -22,11 +22,11 @@ const Hero = () => {
 
   return (
     // Main Container
-    <div className=" flex flex-row  h-full ">
+    <div className=" flex flex-col sm:flex-row  h-full ">
 
 
       {/* left side    */}
-      <div className="flex flex-col gap-10  w-[50%] pl-[77px]  mt-[120px]">
+      <div className="flex flex-col gap-10  sm:w-[50%] md:pl-[77px]  smmt-[120px]">
         {/* Tagline */}
         <div className=" font-manrope font-bold">
           Connecting You to Experts. <br />{" "}
@@ -43,7 +43,7 @@ const Hero = () => {
         </div>
         {/* Button and contact */}
         <div className="flex flex-row gap-10">
-            <Button styles={' h-[56px] , w-[227px] , text-base '} children={"Book an Appointment"}/>
+            <Button styles={' h-[56px] , w-[227px] , text-base , sm:text-[14px] '} children={"Book an Appointment"}/>
 
             <div className=" flex gap-4 items-center">
                 <div className="border-gradient-btn h-[55px] w-[55px] rounded-[20%]  p-[1px] ">
@@ -58,7 +58,7 @@ const Hero = () => {
             </div>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2  gap-8 place-items-center w-[25vw]">
+        <div className="grid grid-cols-2 gap-8 place-items-center sm:w-[25vw]">
             {/* Info 1 */}
             <div className="flex flex-col items-center text-Neutral-900">
               <div className="font-manrope font-bold text-5xl">
@@ -87,17 +87,17 @@ const Hero = () => {
 
 
       {/* right side */}
-      <div className=" flex flex-col justify-center items-center  w-[50%]">
+      <div className=" flex flex-col justify-center items-center  sm:w-[50%]">
         {/* Background Circle and Image */}
-          <div className="absolute w-[396px] h-[402px] bg-ImgBack-gradient rounded-t-[300px]">
+          <div className="absolute sm:w-[396px] sm:h-[402px] w-[310px] h-[378px] bg-ImgBack-gradient rounded-t-[300px]">
           </div>
 
           <div >
-            <img src={HeroImg} alt=""  className=" relative   w-[482px] h-[476px] left-4 top-[123px]  " />
+            <img src={HeroImg} alt=""  className=" relative  sm:w-[482px] sm:h-[476px] w-[382px] h-[376px] left-4 sm:top-[123px]  " />
           </div>
 
         {/* Comment Section */}
-       <div className=" relative w-[241px] h-[128px] bg-white border-2 border-[#95DDFF] rounded-xl shadow-md p-4  flex flex-col justify-between left-44 bottom-44 ">
+       <div className=" hidden relative w-[241px] h-[128px] bg-white border-2 border-[#95DDFF] rounded-xl shadow-md p-4  sm:flex flex-col justify-between left-44 bottom-44 ">
           {/* Top: Images + Number */}
           <div className="flex items-center justify-between">
             {/* Images */}
@@ -139,14 +139,14 @@ const Hero = () => {
         </div>
 
         {/* comment two */}
-        <div className=" relative flex justify-center items-center gap-2 w-[296px] h-[44px] rounded-[10px] bg-white shadow-md bottom-44 right-40">
+        <div className=" hidden relative sm:flex justify-center items-center gap-2 w-[296px] h-[44px] rounded-[10px] bg-white shadow-md bottom-44 right-40">
               <div className=" flex justify-center items-center w-[28px] h-[28px] bg-Primary-Blue-50"> <FaStar className="text-Primary-Blue-700 text-base  "/> </div>
               <div className=" font-manrope font-bold text-base">Easy Appointment Booking</div>
         </div>
 
         {/* comment Three */}
-              <span className=" relative bottom-28 text-Primary-Blue-500 text-6xl right-2 z-10  "><BsQuote/></span>
-        <div className=" relative flex justify-center items-center gap-2 w-[300px] h-[88px] rounded-[10px] bg-white border-2 border-[#95DDFF] shadow-md left-28 bottom-36">
+              <span className=" hidden sm:flex relative bottom-28 text-Primary-Blue-500 text-6xl right-2 z-10  "><BsQuote/></span>
+        <div className=" relative hidden sm:flex justify-center items-center gap-2 w-[300px] h-[88px] rounded-[10px] bg-white border-2 border-[#95DDFF] shadow-md left-28 bottom-36">
               <div className=" text-center  text-base ">Lorem ipsum dolor sit amet, ligula ego. consectetuer adipiscing elit doloras. </div>
         </div>
 
