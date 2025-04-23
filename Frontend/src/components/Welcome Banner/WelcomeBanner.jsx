eb8337f44b49242f8a31d32bead713921d21e460
Swapnil Shinde <swapnil.s@inmedbharat.com>
2025-04-23 14:01:33 +0530

    welcome

--- Frontend/src/components/Welcome Banner/WelcomeBanner.jsx
+++ Frontend/src/components/Welcome Banner/WelcomeBanner.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function WelcomeBanner() {
   return (
-    <section aria-labelledby='WelcomeBanner' className='bg-red-500 p-10 min-h-[500px]'>
-        <div className='bg-white'>
+    <section aria-labelledby='WelcomeBanner' className='p-10 pt-0 min-h-[500px]'>
+        <div className='bg-Primary-Blue-100 flex flex-row h-[450px] rounded-xl p-10'>
             {/* Left */}
-            <div className="w-[50%] bg-yellow-200"> 
+            <div className="w-[50%] bg-yellow-200 "> 
                 <div className="">
                     <div className="">
                         <img src="" alt="" />
