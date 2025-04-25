d11850135518b020878d35d265ed7a30191d0570
Tejas Babar <tejas.b@inmedbharat.com>
2025-04-25 12:00:14 +0530

    respponsiveness for  welcome banner

--- Frontend/src/components/Welcome Banner/WelcomeBanner.jsx
+++ Frontend/src/components/Welcome Banner/WelcomeBanner.jsx
@@ -8,9 +8,9 @@ function WelcomeBanner() {
       aria-labelledby="WelcomeBanner"
       className="p-2 md:p-10  pt-0 min-h-[500px]"
     >
-      <div className="bg-Primary-Blue-100 flex flex-col-reverse gap-4 md:flex-row min-h-[450px] rounded-xl p-4 md:p-10">
+      <div className=" bg-Primary-Blue-100 flex flex-col-reverse justify-center items-center gap-4 lg:flex-row min-h-[450px] rounded-xl p-4 md:p-10">
         {/* Left */}
-        <div className="md:w-[50%] flex justify-start ml-5 items-center">
+        <div className="lg:w-[50%] flex justify-start ml-5 items-center">
           <div className="   bg-Primary-Blue-400  rounded-[3%]">
             <div className=" relative top-7 right-7 ">
               <img src={img} alt="" />
@@ -18,8 +18,8 @@ function WelcomeBanner() {
           </div>
         </div>
         {/* Right */}
-        <div className="md:w-[50%] flex justify-center items-start flex-col gap-8 md:p-8 font-general-sans ">
-          <div className="text-[42px]  font-semibold text-[#011632] ">
+        <div className="lg:w-[50%] flex justify-center items-start flex-col gap-8 md:p-8 font-general-sans ">
+          <div className="text-[36px] sm:text-[42px]  font-semibold text-[#011632] ">
             Start Your Health Journey with Drs-4You.com
           </div>
           <div className=" text-[18px]  leading-8  text-justify ">
@@ -27,18 +27,18 @@ function WelcomeBanner() {
             help.Drs-4You makes scheduling your consultation quick and easy.
             Let’s guide you on the path to better health.
           </div>
-          <div className=" flex flex-row">
-          <div className="flex items-center min-w-auto h-[55px] border border-[#cecece] bg-white rounded-l-md px-3">
-            <BsTelephone className="text-gray-500 mr-3 text-xl" />
-            <input
-                type="text"
-                className="flex-1 bg-transparent text-sm outline-none"
-                placeholder="Enter Your Phone Number"
-            />
-          </div>
-            <div className=" flex justify-center items-center w-[117px] h-[55] bg-[#1376F8] rounded-r-md text-semibold text-white text-base">
-              Submit
+          <div className=" flex flex-row justify-center  ">
+            <div className="flex items-center  h-[55px] border border-[#cecece] bg-white rounded-l-md px-3">
+              <BsTelephone className="text-gray-500 mr-3 text-xl" />
+              <input
+                  type="text"
+                  className="flex-1 bg-transparent text-sm outline-none"
+                  placeholder="Enter Your Phone Number"
+              />
             </div>
+              <div className=" flex justify-center items-center  w-[117px] h-[55] bg-[#1376F8] rounded-r-md text-semibold text-white text-base">
+                Submit
+              </div>
           </div>
         </div>
       </div>
