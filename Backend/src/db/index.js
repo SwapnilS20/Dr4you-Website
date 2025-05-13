3233582b1efc9a14248de9c0c61a8c2d8e8099f4
Swapnil Shinde <swapnil.s@inmedbharat.com>
2025-05-13 16:50:45 +0530

    utils

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -1,14 +1,14 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
+import { DB_NAME } from '../constants.js';
 
 dotenv.config();
 // Create a connection pool to the MySQL database
-console.log(process.env.DB_HOST, process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_NAME, process.env.DB_PORT);
 const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME || DB_NAME,
     port: process.env.DB_PORT,
     connectionLimit: 10,
     waitForConnections: true,
