3ab3425e25f67c382d09acfb589bfa74a4b63241
Tejas Babar <tejas.b@inmedbharat.com>
2025-05-14 17:06:15 +0530

    editor added

--- Frontend/src/Pages/SpecialtiesPages/SpecialtiesPage.jsx
+++ Frontend/src/Pages/SpecialtiesPages/SpecialtiesPage.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
-import Header from "../components/Header/Header.jsx";
-import SpecialitiesCard from "../components/Specialities/SpecialitiesCard.jsx";
+import Header from "../../components/Header/Header.jsx";
+import SpecialitiesCard from "../../components/Specialities/SpecialitiesCard.jsx";
 import { BsArrowLeft } from "react-icons/bs";
 import ReactPaginate from "react-paginate";
 import { motion, AnimatePresence } from "framer-motion";
-import AppointmentForm from "../components/AppointmentForm/AppointmentForm.JSX";
-import Specialist from '../components/Specialist/Specialist.jsx';
-import FrequentlyAskedQuestion from '../components/FAQ/FrequentlyAskedQuestion.jsx';
-import Footer from '../components/Footer/Footer.jsx';
+import AppointmentForm from "../../components/AppointmentForm/AppointmentForm.JSX";
+import Specialist from "../../components/Specialist/Specialist.jsx";
+import FrequentlyAskedQuestion from "../../components/FAQ/FrequentlyAskedQuestion.jsx";
+import Footer from "../../components/Footer/Footer.jsx";
+import MyEditor from "../../components/MyEditor.jsx";
 
 
 
@@ -191,75 +192,78 @@ const SpecialtiesPage = () => {
   };
   return (
     <>
-      <Header />
       {/* cards section */}
       <section>
-        <div className=" flex flex-col gap-10 py-14">
-          {/* Name and Description */}
-          <div className=" flex flex-col justify-center items-center gap-4 mt-8 ">
-            <h1 className=" text-gradient-btn font-general-sans font-semibold text-6xl ">
-              Services{" "}
-            </h1>
-            <p className=" max-w-[550px] text-center">
-              We connect you with trusted healthcare specialists to help you
-              make informed decisions for your well-being.
-            </p>
-          </div>
-          {/* Services Cards */}
-          <AnimatePresence mode="wait">
-            <motion.div
-              key={currentPage}
-              className="grid xs:px-4 md:px-10 px-6 lg:px-10 xl:px-48 gap-8 mt-4 xl:grid-cols-3 2xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1"
-              variants={container}
-              initial="hidden"
-              animate="show"
-              exit="hidden"
-            >
-              {currentItems.map((data) => (
-                <motion.div key={data.id} variants={card}>
-                  <SpecialitiesCard
-                    category={data.category}
-                    description={data.description}
-                    doctors={data.doctors}
-                  />
-                </motion.div>
-              ))}
-            </motion.div>
-          </AnimatePresence>
-
-          {/* Pagination Buttons for mobile */}
-          <div className="flex justify-center ">
-            <ReactPaginate
-              previousLabel={
-                <div className="w-14 h-14 rounded-full border border-Primary-Blue-700 flex items-center justify-center">
-                  <BsArrowLeft className="text-Primary-Blue-700 text-2xl" />
-                </div>
-              }
-              nextLabel={
-                <div className="w-14 h-14 rounded-full border border-Primary-Blue-700 flex items-center justify-center">
-                  <BsArrowLeft className="rotate-180 text-Primary-Blue-700 text-2xl" />
-                </div>
-              }
-              pageCount={pageCount}
-              onPageChange={handlePageClick}
-              containerClassName="flex gap-2 items-center mt-6"
-              pageClassName="show show text-sm" // Hide default number buttons
-              breakClassName="hidden"
-              activeClassName=" w-5 h-5 flex justify-center items-center text-sm rounded-full bg-Primary-Blue-400 text-white"
-              disabledClassName="opacity-50 pointer-events-none"
-              marginPagesDisplayed={2}
-              pageRangeDisplayed={1}
-            />
+        <div className="bg-custom-gradient ">
+          <Header />
+          <div className=" flex flex-col gap-10 py-14 ">
+            {/* Name and Description */}
+            <div className=" flex flex-col justify-center items-center gap-4 mt-8 ">
+              <h1 className=" text-gradient-btn font-general-sans font-semibold text-6xl ">
+                Services{" "}
+              </h1>
+              <p className=" max-w-[550px] text-center">
+                We connect you with trusted healthcare specialists to help you
+                make informed decisions for your well-being.
+              </p>
+            </div>
+            {/* Services Cards */}
+            <AnimatePresence mode="wait">
+              <motion.div
+                key={currentPage}
+                className="grid xs:px-4 md:px-10 px-6 lg:px-10 xl:px-48 gap-8 mt-4 xl:grid-cols-3 2xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1"
+                variants={container}
+                initial="hidden"
+                animate="show"
+                exit="hidden"
+              >
+                {currentItems.map((data) => (
+                  <motion.div key={data.id} variants={card}>
+                    <SpecialitiesCard
+                      category={data.category}
+                      description={data.description}
+                      doctors={data.doctors}
+                    />
+                  </motion.div>
+                ))}
+              </motion.div>
+            </AnimatePresence>
+            {/* Pagination Buttons for mobile */}
+            <div className="flex justify-center ">
+              <ReactPaginate
+                previousLabel={
+                  <div className="w-14 h-14 rounded-full border border-Primary-Blue-700 flex items-center justify-center">
+                    <BsArrowLeft className="text-Primary-Blue-700 text-2xl" />
+                  </div>
+                }
+                nextLabel={
+                  <div className="w-14 h-14 rounded-full border border-Primary-Blue-700 flex items-center justify-center">
+                    <BsArrowLeft className="rotate-180 text-Primary-Blue-700 text-2xl" />
+                  </div>
+                }
+                pageCount={pageCount}
+                onPageChange={handlePageClick}
+                containerClassName="flex gap-2 items-center mt-6"
+                pageClassName="show show text-sm" // Hide default number buttons
+                breakClassName="hidden"
+                activeClassName=" w-5 h-5 flex justify-center items-center text-sm rounded-full bg-Primary-Blue-400 text-white"
+                disabledClassName="opacity-50 pointer-events-none"
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={1}
+              />
+            </div>
           </div>
         </div>
         {/* appointment form */}
         <AppointmentForm />
         {/* Doctors info  */}
-        <Specialist/>
+        <MyEditor />
+      
+        <Specialist />
         {/* FAQ */}
-        <FrequentlyAskedQuestion/>
+        <FrequentlyAskedQuestion />
         {/* footer */}
-        <Footer/>
+        <Footer />
       </section>
     </>
   );
