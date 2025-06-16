59422663e989f5810b7920ce6253698b50a38657
Swapnil Shinde <swapnil.s@inmedbharat.com>
2025-06-16 16:47:25 +0530

    feat: update header and footer schema, add new attributes and components

--- CMS/types/generated/components.d.ts
+++ CMS/types/generated/components.d.ts
@@ -5,7 +5,17 @@ export interface GlobalFooter extends Struct.ComponentSchema {
   info: {
     displayName: 'Footer';
   };
-  attributes: {};
+  attributes: {
+    copyright: Schema.Attribute.String;
+    description: Schema.Attribute.String;
+    logo: Schema.Attribute.Media<'images' | 'files'>;
+    policy_links: Schema.Attribute.Component<'shared.link', true>;
+    social_icons: Schema.Attribute.Component<
+      'shared.social-media-icon-links',
+      true
+    >;
+    support: Schema.Attribute.Component<'shared.link', true>;
+  };
 }
 
 export interface GlobalNavbar extends Struct.ComponentSchema {
@@ -30,12 +40,40 @@ export interface ItemsNavbarItems extends Struct.ComponentSchema {
   };
 }
 
+export interface SharedLink extends Struct.ComponentSchema {
+  collectionName: 'components_shared_links';
+  info: {
+    displayName: 'Link';
+    icon: 'attachment';
+  };
+  attributes: {
+    target: Schema.Attribute.Enumeration<
+      ['_blank', '_self', '_parent', '_top']
+    >;
+    text: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
+export interface SharedSocialMediaIconLinks extends Struct.ComponentSchema {
+  collectionName: 'components_shared_social_media_icon_links';
+  info: {
+    displayName: 'social_media_icon_links';
+  };
+  attributes: {
+    icon: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    links: Schema.Attribute.Component<'shared.link', false>;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
       'global.footer': GlobalFooter;
       'global.navbar': GlobalNavbar;
       'items.navbar-items': ItemsNavbarItems;
+      'shared.link': SharedLink;
+      'shared.social-media-icon-links': SharedSocialMediaIconLinks;
     }
   }
 }
