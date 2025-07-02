5b33eb9279f1e02fb437ab300f37dd74a697f03f
Swapnil Shinde <swapnil.s@inmedbharat.com>
2025-07-02 16:52:35 +0530

    fix: update CMS server URL to include the correct path

--- CMS/config/server.js
+++ CMS/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'https://drs-4you.com'),
+  url: env('URL', 'https://drs-4you.com/cms'),
   app: {
     keys: env.array('APP_KEYS'),
   },
